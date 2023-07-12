aa1c6304929b48b337eaf900920a68ea568ca21b
sadvika7reddy <sadvika7reddy@gmail.com>
2023-07-12 15:38:06 +0530

    problem1

---
+++ problem1.js
@@ -0,0 +1,7 @@
+let inventory=require('./inventory.js');
+
+function problem1(inform){
+    console.log(`car 33 is a ${inform[32].car_year} ${inform[32].car_make} ${inform[32].car_model}`)
+
+}
+problem1(inventory)
